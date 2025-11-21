4eef677269aecce969f3c306c7fc413a6ebe61ca
highperfocused <highperfocused@pm.me>
2025-11-21 23:10:00 +0100

    Set basename for BrowserRouter and update Vite config for production mode

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -8,7 +8,7 @@ import Manager from "./pages/Manager";
 
 export function AppRouter() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <ScrollToTop />
       <Routes>
         <Route path="/" element={<Index />} />
